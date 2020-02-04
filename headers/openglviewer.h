2c813ef891ffb8c434a5136b0304d77c5cf519fb
Tony <erlinmaquer@gmail.com>
2020-02-05 01:43:41 +0200

    [WorkingViewer] OpenGl viewer show standart cube. Have posibility to rotate it.

--- headers/openglviewer.h
+++ headers/openglviewer.h
@@ -1,59 +1,38 @@
 #ifndef OPENGLVIEWER_H
 #define OPENGLVIEWER_H
 
-//#include <QWidget>
-#include <QOpenGLWidget>
+#include <QGLWidget>
+#include <QMouseEvent>
 #include <QOpenGLFunctions>
-#include <QMatrix4x4>
-#include <QQuaternion>
-#include <QVector2D>
-#include <QBasicTimer>
-#include <QOpenGLShaderProgram>
-#include <QOpenGLTexture>
+#include <QOpenGLWidget>
+//#include <QtOpenGL>
+//#include <QOpenGLShaderProgram>
+//#include <QOpenGLTexture>
 namespace Ui {
 class OpenGlViewer;
 }
 
-class OpenGlViewer : public QOpenGLWidget, protected QOpenGLFunctions
-{
-    Q_OBJECT
+class OpenGlViewer : public QGLWidget, protected QOpenGLFunctions {
+  Q_OBJECT
 
-public:
-    explicit OpenGlViewer(QWidget *parent = nullptr);
-    ~OpenGlViewer();
+ public:
+  explicit OpenGlViewer(QWidget *parent = nullptr);
+  ~OpenGlViewer() override;
 
-protected:
-    void initializeGL() override;
-    void resizeGL(int w, int h) override;
-    void paintGL() override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *e) override;
-    void timerEvent(QTimerEvent *e) override;
+ protected:
+  void initializeGL() override;
+  void resizeGL(int w, int h) override;
+  void paintGL() override;
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;
+  void mouseReleaseEvent(QMouseEvent *e) override;
+  // void timerEvent(QTimerEvent *e) override;
 
-private:
-    Ui::OpenGlViewer *ui;
-    QBasicTimer timer;
-    QOpenGLShaderProgram program;
+ private:
+  int openGlwidth, openGlheight;  // window size
 
-    QOpenGLTexture *texture;
-
-    QMatrix4x4 projection;
-
-    QVector2D mousePressPosition;
-    QVector3D rotationAxis;
-    QOpenGLFunctions *geom;
-    qreal angularSpeed;
-    QQuaternion rotation;
-    // QtLogo *logo;
-    double x_pos; // position mouse on x
-    double y_pos;
-    int xRot;
-    int yRot;
-    int zRot;
-    QPoint lastPos;
-    QColor qtGreen;
-    QColor qtPurple;
+  float x_pos, y_pos, rotate_y, rotate_x;  // rotate values
+  Ui::OpenGlViewer *ui;
 };
 
-#endif // OPENGLVIEWER_H
+#endif  // OPENGLVIEWER_H
--- sources/openglviewer.cpp
+++ sources/openglviewer.cpp
@@ -1,132 +1,121 @@
 #include "headers/openglviewer.h"
+
 #include "ui_openglviewer.h"
 
-double scale_x = 3;                         // that need to zoom in or zoom out
-double scale_y = 3;                         // that need to zoom in or zoom out
-double scale_z = 3;                         // that need to zoom in or zoom out
-double rotate_y = 0;                        // that need for rotate Object
-double rotate_x = 0;                        // that need for rotate Object
-double rotate_z = 0;                        // that need for rotate Object
-std::vector<std::vector<double>> vector_3d; // vector that consist all vectors that we will draw
-OpenGlViewer::OpenGlViewer(QWidget *parent)
-    : QOpenGLWidget(parent)
-{
-    ui->setupUi(this);
+OpenGlViewer::OpenGlViewer(QWidget *parent) : QGLWidget(parent) {
+  ui->setupUi(this);
+
+  openGlwidth = 500;                        // window width
+  openGlheight = 500;                       // window height
+  setFormat(QGLFormat(QGL::DoubleBuffer));  // double buff
 }
 
-OpenGlViewer::~OpenGlViewer()
-{
-    delete ui;
+OpenGlViewer::~OpenGlViewer() { delete ui; }
+
+void OpenGlViewer::initializeGL() {
+  initializeOpenGLFunctions();
+  glDepthFunc(GL_LEQUAL);    // buff deep
+  qglClearColor(Qt::black);  // set background black
 }
 
-void OpenGlViewer::initializeGL()
-{
-
-    initializeOpenGLFunctions();
-
-    glClearColor(0, 0, 0, 1);
-
-    // initShaders();
-    // initTextures();
-
-    //! [2]
-    // Enable depth buffer
-    glEnable(GL_DEPTH_TEST);
-
-    // Enable back face culling
-    glEnable(GL_CULL_FACE);
+void OpenGlViewer::resizeGL(int w, int h) {
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glViewport(0, 0, (GLint)w, (GLint)h);
+  openGlwidth = w;
+  openGlheight = h;
 }
 
-void OpenGlViewer::resizeGL(int w, int h)
-{
-    // Calculate aspect ratio
-    qreal aspect = qreal(w) / qreal(h ? h : 1);
+void OpenGlViewer::paintGL() {
+  glClear(GL_COLOR_BUFFER_BIT |
+          GL_DEPTH_BUFFER_BIT);  // clear buff image and deep
+  glMatrixMode(GL_PROJECTION);   // set the matrix
+  glShadeModel(GL_SMOOTH);
+  glLoadIdentity();  // load matrix
+  glOrtho(-openGlwidth, openGlwidth, openGlheight, -openGlheight, -500,
+          500);  // set matrix scope (x,y,z)
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // Set near plane to 3.0, far plane to 7.0, field of view 45 degrees
-    const qreal zNear = 3.0, zFar = 7.0, fov = 45.0;
+  glRotatef(rotate_x, 1.0, 0.0, 0.0);  // rotate x
+  glRotatef(rotate_y, 0.0, 1.0, 0.0);  // rotate y
 
-    // Reset projection
-    projection.setToIdentity();
+  float value = 100;
 
-    // Set perspective projection
-    projection.perspective(fov, aspect, zNear, zFar);
+  glBegin(GL_QUADS);
+  glVertex2f(-value / 4, value / 4);   // vertex 1
+  glVertex2f(-value / 2, -value / 4);  // vertex 2
+  glVertex2f(value / 2, -value / 4);   // vertex 3
+  glVertex2f(value / 4, value / 4);    // vertex 4
+  glEnd();
+
+  glBegin(GL_QUADS);  // Begin drawing the color cube with 6 quads
+
+  glColor3f(value, value, 0.0f);  // Green
+  glVertex3f(value, value, -value);
+  glVertex3f(-value, value, -value);
+  glVertex3f(-value, value, value);
+  glVertex3f(value, value, value);
+
+  // Bottom face (y = -value)
+  glColor3f(value, value / 2, 0.0f);  // Orange
+  glVertex3f(value, -value, value);
+  glVertex3f(-value, -value, value);
+  glVertex3f(-value, -value, -value);
+  glVertex3f(value, -value, -value);
+
+  // Front face  (z = value)
+  glColor3f(value, 0.0f, 0.0f);  // Red
+  glVertex3f(value, value, value);
+  glVertex3f(-value, value, value);
+  glVertex3f(-value, -value, value);
+  glVertex3f(value, -value, value);
+
+  // Back face (z = -value)
+  glColor3f(value, value, 0.0f);  // Yellow
+  glVertex3f(value, -value, -value);
+  glVertex3f(-value, -value, -value);
+  glVertex3f(-value, value, -value);
+  glVertex3f(value, value, -value);
+
+  // Left face (x = -value)
+  glColor3f(0.0f, 0.0f, value);  // Blue
+  glVertex3f(-value, value, value);
+  glVertex3f(-value, value, -value);
+  glVertex3f(-value, -value, -value);
+  glVertex3f(-value, -value, value);
+
+  // Right face (x = value)
+  glColor3f(value, 0.0f, value);  // Magenta
+  glVertex3f(value, value, -value);
+  glVertex3f(value, value, value);
+  glVertex3f(value, -value, value);
+  glVertex3f(value, -value, -value);
+  glEnd();  // End of drawing color-cube
+
+  doubleBuffer();
 }
 
-void OpenGlViewer::paintGL()
-{
-
-    // Clear color and depth buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    texture->bind();
-
-    //! [6]
-    // Calculate model view transformation
-    QMatrix4x4 matrix;
-    matrix.translate(0.0, 0.0, -5.0);
-    matrix.rotate(rotation);
-
-    // Set modelview-projection matrix
-    program.setUniformValue("mvp_matrix", projection * matrix);
-    //! [6]
-
-    // Use texture unit 0 which contains cube.png
-    program.setUniformValue("texture", 0);
-
-    // Draw cube geometry
-    // geom->drawCubeGeometry(&program);
+void OpenGlViewer::mousePressEvent(QMouseEvent *event) {
+  if (event->button() ==
+      Qt::LeftButton) {  // memorize coords x and y mouse when we start clicking
+    x_pos = event->x();
+    y_pos = event->y();
+  }
 }
 
-void OpenGlViewer::mousePressEvent(QMouseEvent *event)
-{
-    mousePressPosition = QVector2D(event->localPos());
+void OpenGlViewer::mouseMoveEvent(QMouseEvent *event) {
+  if (event->buttons() & Qt::LeftButton) {
+    rotate_y = event->x() - x_pos;  // rotate Object on x
+    rotate_x = event->y() - y_pos;  // rotate Object on y
+    update();                       // update Form that display Object
+  }
 }
 
-void OpenGlViewer::mouseMoveEvent(QMouseEvent *event)
-{
-    if (event->buttons() & Qt::LeftButton)
-    {
-        rotate_y = event->x() - x_pos; // rotate Object on x
-        rotate_x = event->y() - y_pos; // rotate Object on y
-        this->update();                // update Form that display Object
-    }
+void OpenGlViewer::mouseReleaseEvent(QMouseEvent *e) {
+  x_pos = 0;
+  y_pos = 0;
+  // Mouse release position - mouse press position
 }
 
-void OpenGlViewer::mouseReleaseEvent(QMouseEvent *e)
-{
-    // Mouse release position - mouse press position
-    QVector2D diff = QVector2D(e->localPos()) - mousePressPosition;
-
-    // Rotation axis is perpendicular to the mouse position difference
-    // vector
-    QVector3D n = QVector3D(diff.y(), diff.x(), 0.0).normalized();
-
-    // Accelerate angular speed relative to the length of the mouse sweep
-    qreal acc = diff.length() / 100.0;
-
-    // Calculate new rotation axis as weighted sum
-    rotationAxis = (rotationAxis * angularSpeed + n * acc).normalized();
-
-    // Increase angular speed
-    angularSpeed += acc;
-}
-
-void OpenGlViewer::timerEvent(QTimerEvent *e)
-{
-    // Decrease angular speed (friction)
-    angularSpeed *= 0.99;
-
-    // Stop rotation when speed goes below threshold
-    if (angularSpeed < 0.01)
-    {
-        angularSpeed = 0.0;
-    }
-    else
-    {
-        // Update rotation
-        rotation = QQuaternion::fromAxisAndAngle(rotationAxis, angularSpeed) * rotation;
-
-        // Request an update
-        update();
-    }
-}
+// void OpenGlViewer::timerEvent(QTimerEvent *e) {}
