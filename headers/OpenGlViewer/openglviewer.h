7da47009aebf295b6af0934a5cc920002414dcd5
Tony <erlinmaquer@gmail.com>
2020-02-11 00:57:12 +0200

    [OpenGlViewer] Change color style, turn off the grid  and some not important addition.

--- headers/OpenGlViewer/openglviewer.h
+++ headers/OpenGlViewer/openglviewer.h
@@ -15,49 +15,50 @@ namespace Ui {
 class OpenGlViewer;
 }
 
-class OpenGlViewer : public QGLWidget, protected QOpenGLFunctions
-{
-    Q_OBJECT
+class OpenGlViewer : public QGLWidget, protected QOpenGLFunctions {
+  Q_OBJECT
 
-public:
-    explicit OpenGlViewer(Object3d _object, QWidget *parent = nullptr);
-    ~OpenGlViewer() override;
+ public:
+  explicit OpenGlViewer(Object3d _object, QWidget *parent = nullptr);
+  ~OpenGlViewer() override;
 
-protected:
-    void initializeGL() override;
-    void resizeGL(int w, int h) override;
-    void paintGL() override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override; // object roration
-    void mouseReleaseEvent(QMouseEvent *e) override;
-    void wheelEvent(QWheelEvent *event) override; // object scale
+ protected:
+  void initializeGL() override;
+  void resizeGL(int w, int h) override;
+  void paintGL() override;
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;  // object roration
+  void mouseReleaseEvent(QMouseEvent *e) override;
+  void wheelEvent(QWheelEvent *event) override;  // object scale
 
-    // void timerEvent(QTimerEvent *e) override;
-private:
-    void drawGrid();
+  // void timerEvent(QTimerEvent *e) override;
+ private:
+  void drawGrid();
 
-    void drawLines();
-    void drawTriangles();
-    void drawSquads();
+  void drawLines();
+  void drawTriangles();
+  void drawSquads();
 
-private:
-    QElapsedTimer *timerForTest;
+ private:
+  QElapsedTimer *timerForTest;
 
-    int openGlwidth, openGlheight;          // window size
-    Object3d drawObject;                    // object that need to draw
-    float x_pos, y_pos, rotate_y, rotate_x; // rotate values
-    int scaleWheel = 2;                     // current scale (zoom in\zoom out)
-    Ui::OpenGlViewer *ui;
+  int openGlwidth, openGlheight;           // window size
+  Object3d drawObject;                     // object that need to draw
+  float x_pos, y_pos, rotate_y, rotate_x;  // rotate values
+  int scaleWheel = 2;                      // current scale (zoom in\zoom out)
+  Ui::OpenGlViewer *ui;
 
-    std::vector<primitives::Point3d> vertexData;
+  std::vector<primitives::Point3d> vertexData;
 
-    std::vector<primitives::Line3d> linesData; // load lines data from object
+  std::vector<primitives::Line3d> linesData;  // load lines data from object
 
-    std::vector<primitives::Triangle3d> trianglesData; // get triangles data from object
-    std::vector<primitives::Face3d> facesData;         // load faces from object
-    uint vertexVSize, linesVSize, trianglesVSize, facesVSize;
+  std::vector<primitives::Triangle3d>
+      trianglesData;                          // get triangles data from object
+  std::vector<primitives::Face3d> facesData;  // load faces from object
+  uint vertexVSize, linesVSize, trianglesVSize,
+      facesVSize;  // quantity vertex/lines/triangles/faces
 
-    int maxOrigin;
+  int maxOrigin;  // max value of coords x/y/z
 };
 
-#endif // OPENGLVIEWER_H
+#endif  // OPENGLVIEWER_H
--- sources/OpenGlViewer/openglviewer.cpp
+++ sources/OpenGlViewer/openglviewer.cpp
@@ -5,263 +5,259 @@
 #include "ui_openglviewer.h"
 
 OpenGlViewer::OpenGlViewer(Object3d _object, QWidget *parent)
-    : QGLWidget(parent)
-{
-    ui->setupUi(this);
-    drawObject = _object;                    // get object that need to draw
-    openGlwidth = 500;                       // window width
-    openGlheight = 500;                      // window height
-    setFormat(QGLFormat(QGL::DoubleBuffer)); // double buff
+    : QGLWidget(parent) {
+  ui->setupUi(this);
+  drawObject = _object;                     // get object that need to draw
+  openGlwidth = 500;                        // window width
+  openGlheight = 500;                       // window height
+  setFormat(QGLFormat(QGL::DoubleBuffer));  // double buff
 
-    vertexData = drawObject.getVertex3dData();            // load vertex data from object
-    linesData = drawObject.getLines3dData();              // load lines data from object
-    trianglesData = drawObject.getTrianglesData();        // get triangles data from object
-    facesData = drawObject.getFaces3dData();              // load faces from object
-    vertexVSize = drawObject.getQuantityVertex3d();       // quantity vertex in current object
-    linesVSize = drawObject.getQuantityLines3d();         // quantity lines in current object
-    trianglesVSize = drawObject.getQuantityTriangles3d(); // quantity triagnles in current object
-    facesVSize = drawObject.getQuantityFaces3d();         // quantity faces in current object
+  vertexData = drawObject.getVertex3dData();  // load vertex data from object
+  linesData = drawObject.getLines3dData();    // load lines data from object
+  trianglesData =
+      drawObject.getTrianglesData();        // get triangles data from object
+  facesData = drawObject.getFaces3dData();  // load faces from object
+  vertexVSize =
+      drawObject.getQuantityVertex3d();  // quantity vertex in current object
+  linesVSize =
+      drawObject.getQuantityLines3d();  // quantity lines in current object
+  trianglesVSize =
+      drawObject
+          .getQuantityTriangles3d();  // quantity triagnles in current object
+  facesVSize =
+      drawObject.getQuantityFaces3d();  // quantity faces in current object
 
-    maxOrigin = drawObject.getMaxOriginAxis(); // get max abs value current object among axis x, y, z
+  maxOrigin = drawObject.getMaxOriginAxis();  // get max abs value current
+                                              // object among axis x, y, z
 
-    timerForTest = new QElapsedTimer();
+  timerForTest = new QElapsedTimer();
 }
 
-OpenGlViewer::~OpenGlViewer()
-{
-    delete ui;
+OpenGlViewer::~OpenGlViewer() { delete ui; }
+
+void OpenGlViewer::initializeGL() {
+  initializeOpenGLFunctions();
+  glDepthFunc(GL_LEQUAL);   // buff deep
+  qglClearColor(Qt::gray);  // set background black
+  glEnable(GL_DEPTH_TEST);  // line that we can't see - become invisible
+
+  //    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);    //to enable
+  //    transparency. In future version uncomment glEnable(GL_BLEND); //to
+  //    enable transparency. In future version uncomment glClearColor(0.0, 0.0,
+  //    0.0, 0.0);           //to enable transparency. In future version
+  //    uncomment
 }
 
-void OpenGlViewer::initializeGL()
-{
+void OpenGlViewer::resizeGL(int w, int h) {
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glViewport(0, 0, (GLint)w, (GLint)h);
+  openGlwidth = w;
+  openGlheight = h;
+}
+void cube() {
+  float value = 100;
 
-    initializeOpenGLFunctions();
-    glDepthFunc(GL_LEQUAL);   // buff deep
-    qglClearColor(Qt::black); // set background black
-    glEnable(GL_DEPTH_TEST);  // line that we can't see - become invisible
+  glBegin(GL_QUADS);
+  glVertex2f(-value / 4, value / 4);   // vertex 1
+  glVertex2f(-value / 2, -value / 4);  // vertex 2
+  glVertex2f(value / 2, -value / 4);   // vertex 3
+  glVertex2f(value / 4, value / 4);    // vertex 4
+  glEnd();
 
-    //    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);    //to enable
-    //    transparency. In future version uncomment glEnable(GL_BLEND); //to
-    //    enable transparency. In future version uncomment glClearColor(0.0, 0.0,
-    //    0.0, 0.0);           //to enable transparency. In future version
-    //    uncomment
+  glBegin(GL_QUADS);  // Begin drawing the color cube with 6 quads
+
+  glColor3f(value, value, 0.0f);  // Green
+  glVertex3f(value, value, -value);
+  glVertex3f(-value, value, -value);
+  glVertex3f(-value, value, value);
+  glVertex3f(value, value, value);
+
+  // Bottom face (y = -value)
+  glColor3f(value, value / 2, 0.0f);  // Orange
+  glVertex3f(value, -value, value);
+  glVertex3f(-value, -value, value);
+  glVertex3f(-value, -value, -value);
+  glVertex3f(value, -value, -value);
+
+  // Front face  (z = value)
+  glColor3f(value, 0.0f, 0.0f);  // Red
+  glVertex3f(value, value, value);
+  glVertex3f(-value, value, value);
+  glVertex3f(-value, -value, value);
+  glVertex3f(value, -value, value);
+
+  // Back face (z = -value)
+  glColor3f(value, value, 0.0f);  // Yellow
+  glVertex3f(value, -value, -value);
+  glVertex3f(-value, -value, -value);
+  glVertex3f(-value, value, -value);
+  glVertex3f(value, value, -value);
+
+  // Left face (x = -value)
+  glColor3f(0.0f, 0.0f, value);  // Blue
+  glVertex3f(-value, value, value);
+  glVertex3f(-value, value, -value);
+  glVertex3f(-value, -value, -value);
+  glVertex3f(-value, -value, value);
+
+  // Right face (x = value)
+  glColor3f(value, 0.0f, value);  // Magenta
+  glVertex3f(value, value, -value);
+  glVertex3f(value, value, value);
+  glVertex3f(value, -value, value);
+  glVertex3f(value, -value, -value);
+  glEnd();  // End of drawing color-cube
+}
+void OpenGlViewer::paintGL() {
+  timerForTest->restart();
+
+  glClear(GL_COLOR_BUFFER_BIT |
+          GL_DEPTH_BUFFER_BIT);  // clear buff image and deep
+  glMatrixMode(GL_PROJECTION);   // set the matrix
+  glShadeModel(GL_SMOOTH);
+  glLoadIdentity();  // load matrix
+
+  glOrtho(-scaleWheel * maxOrigin, scaleWheel * maxOrigin,
+          scaleWheel * maxOrigin, -scaleWheel * maxOrigin,
+          -scaleWheel * maxOrigin,
+          scaleWheel * maxOrigin);  // set matrix scope. Need get opportunity to
+                                    // scale (zoom in\out)
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  // camera rotation
+  glRotatef(rotate_x, 1.0, 0.0, 0.0);  // rotate x
+  glRotatef(rotate_y, 0.0, 1.0, 0.0);  // rotate y
+
+  // cube();
+
+  glColor3f(0.0f, 0.0f, 0.0f);  // filling color (grey)
+  drawSquads();                 // draw squads
+  drawTriangles();              // draw triangles outline
+
+  // DRAW LINES START
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // glPolygonMode(GL_BACK, GL_LINE);
+  glColor3f(0.0f, 8.0f, 8.0f);  // outline color (blue)
+  drawSquads();                 // draw squads outline
+  drawTriangles();              // draw triangles outline
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  // glPolygonMode(GL_BACK, GL_FILL);
+  // LINES
+  //  drawGrid();
+  doubleBuffer();
+  qDebug() << "Time= " << timerForTest->elapsed();
 }
 
-void OpenGlViewer::resizeGL(int w, int h)
-{
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glViewport(0, 0, (GLint)w, (GLint)h);
-    openGlwidth = w;
-    openGlheight = h;
-}
-void cube()
-{
-    float value = 100;
-
-    glBegin(GL_QUADS);
-    glVertex2f(-value / 4, value / 4);  // vertex 1
-    glVertex2f(-value / 2, -value / 4); // vertex 2
-    glVertex2f(value / 2, -value / 4);  // vertex 3
-    glVertex2f(value / 4, value / 4);   // vertex 4
-    glEnd();
-
-    glBegin(GL_QUADS); // Begin drawing the color cube with 6 quads
-
-    glColor3f(value, value, 0.0f); // Green
-    glVertex3f(value, value, -value);
-    glVertex3f(-value, value, -value);
-    glVertex3f(-value, value, value);
-    glVertex3f(value, value, value);
-
-    // Bottom face (y = -value)
-    glColor3f(value, value / 2, 0.0f); // Orange
-    glVertex3f(value, -value, value);
-    glVertex3f(-value, -value, value);
-    glVertex3f(-value, -value, -value);
-    glVertex3f(value, -value, -value);
-
-    // Front face  (z = value)
-    glColor3f(value, 0.0f, 0.0f); // Red
-    glVertex3f(value, value, value);
-    glVertex3f(-value, value, value);
-    glVertex3f(-value, -value, value);
-    glVertex3f(value, -value, value);
-
-    // Back face (z = -value)
-    glColor3f(value, value, 0.0f); // Yellow
-    glVertex3f(value, -value, -value);
-    glVertex3f(-value, -value, -value);
-    glVertex3f(-value, value, -value);
-    glVertex3f(value, value, -value);
-
-    // Left face (x = -value)
-    glColor3f(0.0f, 0.0f, value); // Blue
-    glVertex3f(-value, value, value);
-    glVertex3f(-value, value, -value);
-    glVertex3f(-value, -value, -value);
-    glVertex3f(-value, -value, value);
-
-    // Right face (x = value)
-    glColor3f(value, 0.0f, value); // Magenta
-    glVertex3f(value, value, -value);
-    glVertex3f(value, value, value);
-    glVertex3f(value, -value, value);
-    glVertex3f(value, -value, -value);
-    glEnd(); // End of drawing color-cube
-}
-void OpenGlViewer::paintGL()
-{
-    timerForTest->restart();
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear buff image and deep
-    glMatrixMode(GL_PROJECTION);                        // set the matrix
-    glShadeModel(GL_SMOOTH);
-    glLoadIdentity(); // load matrix
-
-    glOrtho(-scaleWheel * maxOrigin, scaleWheel * maxOrigin, scaleWheel * maxOrigin, -scaleWheel * maxOrigin,
-            -scaleWheel * maxOrigin,
-            scaleWheel * maxOrigin); // set matrix scope. Need get opportunity to
-                                     // scale (zoom in\out)
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    // camera rotation
-    glRotatef(rotate_x, 1.0, 0.0, 0.0); // rotate x
-    glRotatef(rotate_y, 0.0, 1.0, 0.0); // rotate y
-
-    // cube();
-
-    glColor3f(0.795f, 0.795f, 0.795f); // filling color (grey)
-    drawSquads();                      // draw squads
-    drawTriangles();                   // draw triangles outline
-
-    // DRAW LINES START
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    // glPolygonMode(GL_BACK, GL_LINE);
-    glColor3f(0.0f, 0.296f, 0.695f); // outline color (blue)
-    drawSquads();                    // draw squads outline
-    drawTriangles();                 // draw triangles outline
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    // glPolygonMode(GL_BACK, GL_FILL);
-    // LINES
-    drawGrid();
-    doubleBuffer();
-    qDebug() << "Time= " << timerForTest->elapsed();
+void OpenGlViewer::mousePressEvent(QMouseEvent *event) {
+  if (event->button() ==
+      Qt::LeftButton) {  // memorize coords x and y mouse when we start clicking
+    x_pos = event->x();
+    y_pos = event->y();
+  }
 }
 
-void OpenGlViewer::mousePressEvent(QMouseEvent *event)
-{
-    if (event->button() == Qt::LeftButton)
-    { // memorize coords x and y mouse when we start clicking
-        x_pos = event->x();
-        y_pos = event->y();
+void OpenGlViewer::mouseMoveEvent(QMouseEvent *event) {
+  if (event->buttons() & Qt::LeftButton) {
+    rotate_y = event->x() - x_pos;  // rotate Object on x
+    rotate_x = event->y() + y_pos;  // rotate Object on y
+    update();                       // update Form that display Object
+  }
+}
+
+void OpenGlViewer::mouseReleaseEvent(QMouseEvent *e) {
+  x_pos = 0;
+  y_pos = 0;
+  // Mouse release position - mouse press position
+}
+
+void OpenGlViewer::wheelEvent(QWheelEvent *event) {
+  scaleWheel +=
+      event->angleDelta().y() / 100;  // change scale when scroll wheel
+  update();
+}
+
+void OpenGlViewer::drawGrid() {
+  int gridScale = 500;  // size (width and height) all grid
+  float gridSize = 50;  // size of one cell of grid
+  glLineWidth(1);       // width grid line
+
+  glBegin(GL_LINES);            // START LINES DRAWING
+  glColor3f(1.0f, 1.0f, 1.0f);  // set line color (WHITE)
+  for (float i = -gridScale; i < gridScale; i += gridSize) {
+    for (float j = -gridScale; j < gridScale; j += gridSize) {
+      // draw cells
+      glVertex2f(i, j);
+      glVertex2f(i + gridSize, j);
+
+      glVertex2f(i, j);
+      glVertex2f(i, j + gridSize);
+
+      glVertex2f(i, j + gridSize);
+      glVertex2f(i + gridSize, j + gridSize);
+
+      glVertex2f(i + gridSize, j + gridSize);
+      glVertex2f(i + gridSize, j);
     }
+  }
+  glEnd();  // END LINES DRAWING
 }
 
-void OpenGlViewer::mouseMoveEvent(QMouseEvent *event)
-{
-    if (event->buttons() & Qt::LeftButton)
-    {
-        rotate_y = event->x() - x_pos; // rotate Object on x
-        rotate_x = event->y() - y_pos; // rotate Object on y
-        update();                      // update Form that display Object
-    }
+void OpenGlViewer::drawLines() {
+  // OLD non oprimized code for lines drawing
+  //  // LINES COMMENTED START
+  glLineWidth(1);     // set line width
+  glBegin(GL_LINES);  // START LINES DRAWING
+  for (uint i = 0; i < linesVSize; ++i) {
+    // draw all lines:
+    glVertex3f(vertexData[linesData[i].indexVertex1].x,
+               vertexData[linesData[i].indexVertex1].y,
+               vertexData[linesData[i].indexVertex1].z);
+    glVertex3f(vertexData[linesData[i].indexVertex2].x,
+               vertexData[linesData[i].indexVertex2].y,
+               vertexData[linesData[i].indexVertex2].z);
+  }
+  glEnd();  // END LINES DRAWING
+            //  // LINES COMMENTED END
 }
 
-void OpenGlViewer::mouseReleaseEvent(QMouseEvent *e)
-{
-    x_pos = 0;
-    y_pos = 0;
-    // Mouse release position - mouse press position
+void OpenGlViewer::drawTriangles() {
+  glBegin(GL_TRIANGLES);  // START TRIANGLES DRAWING
+  for (uint i = 0; i < trianglesVSize; ++i) {
+    // draw all triangles:
+    glVertex3f(vertexData[trianglesData[i].indexVertex1].x,
+               vertexData[trianglesData[i].indexVertex1].y,
+               vertexData[trianglesData[i].indexVertex1].z);
+    glVertex3f(vertexData[trianglesData[i].indexVertex2].x,
+               vertexData[trianglesData[i].indexVertex2].y,
+               vertexData[trianglesData[i].indexVertex2].z);
+    glVertex3f(vertexData[trianglesData[i].indexVertex3].x,
+               vertexData[trianglesData[i].indexVertex3].y,
+               vertexData[trianglesData[i].indexVertex3].z);
+  }
+  glEnd();  // END TRIANGLES DRAWING
 }
 
-void OpenGlViewer::wheelEvent(QWheelEvent *event)
-{
-    scaleWheel += event->angleDelta().y() / 100; // change scale when scroll wheel
-    update();
-}
-
-void OpenGlViewer::drawGrid()
-{
-    float gridSize = 50; // size of one cell of grid
-    int gridScale = 500; // size (width and height) all grid
-    glLineWidth(1);      // width grid line
-
-    glBegin(GL_LINES);           // START LINES DRAWING
-    glColor3f(1.0f, 1.0f, 1.0f); // set line color (WHITE)
-    for (float i = -gridScale; i < gridScale; i += gridSize)
-    {
-        for (float j = -gridScale; j < gridScale; j += gridSize)
-        {
-            // draw cells
-            glVertex2f(i, j);
-            glVertex2f(i + gridSize, j);
-
-            glVertex2f(i, j);
-            glVertex2f(i, j + gridSize);
-
-            glVertex2f(i, j + gridSize);
-            glVertex2f(i + gridSize, j + gridSize);
-
-            glVertex2f(i + gridSize, j + gridSize);
-            glVertex2f(i + gridSize, j);
-        }
-    }
-    glEnd(); // END LINES DRAWING
-}
-
-void OpenGlViewer::drawLines()
-{
-    // OLD non oprimized code for lines drawing
-    //  // LINES COMMENTED START
-    glLineWidth(1);    // set line width
-    glBegin(GL_LINES); // START LINES DRAWING
-    for (uint i = 0; i < linesVSize; ++i)
-    {
-        // draw all lines:
-        glVertex3f(vertexData[linesData[i].indexVertex1].x, vertexData[linesData[i].indexVertex1].y,
-                   vertexData[linesData[i].indexVertex1].z);
-        glVertex3f(vertexData[linesData[i].indexVertex2].x, vertexData[linesData[i].indexVertex2].y,
-                   vertexData[linesData[i].indexVertex2].z);
-    }
-    glEnd(); // END LINES DRAWING
-    //  // LINES COMMENTED END
-}
-
-void OpenGlViewer::drawTriangles()
-{
-    glBegin(GL_TRIANGLES); // START TRIANGLES DRAWING
-    for (uint i = 0; i < trianglesVSize; ++i)
-    {
-        // draw all triangles:
-        glVertex3f(vertexData[trianglesData[i].indexVertex1].x, vertexData[trianglesData[i].indexVertex1].y,
-                   vertexData[trianglesData[i].indexVertex1].z);
-        glVertex3f(vertexData[trianglesData[i].indexVertex2].x, vertexData[trianglesData[i].indexVertex2].y,
-                   vertexData[trianglesData[i].indexVertex2].z);
-        glVertex3f(vertexData[trianglesData[i].indexVertex3].x, vertexData[trianglesData[i].indexVertex3].y,
-                   vertexData[trianglesData[i].indexVertex3].z);
-    }
-    glEnd(); // END TRIANGLES DRAWING
-}
-
-void OpenGlViewer::drawSquads()
-{
-    glBegin(GL_QUADS); // START FACES DRAWING
-    for (uint i = 0; i < facesVSize; ++i)
-    {
-        // draw all squads:
-        glVertex3f(vertexData[facesData[i].indexVertex1].x, vertexData[facesData[i].indexVertex1].y,
-                   vertexData[facesData[i].indexVertex1].z);
-        glVertex3f(vertexData[facesData[i].indexVertex2].x, vertexData[facesData[i].indexVertex2].y,
-                   vertexData[facesData[i].indexVertex2].z);
-        glVertex3f(vertexData[facesData[i].indexVertex3].x, vertexData[facesData[i].indexVertex3].y,
-                   vertexData[facesData[i].indexVertex3].z);
-        glVertex3f(vertexData[facesData[i].indexVertex4].x, vertexData[facesData[i].indexVertex4].y,
-                   vertexData[facesData[i].indexVertex4].z);
-    }
-    glEnd(); // END TRIANGLES DRAWING
+void OpenGlViewer::drawSquads() {
+  glBegin(GL_QUADS);  // START FACES DRAWING
+  for (uint i = 0; i < facesVSize; ++i) {
+    // draw all squads:
+    glVertex3f(vertexData[facesData[i].indexVertex1].x,
+               vertexData[facesData[i].indexVertex1].y,
+               vertexData[facesData[i].indexVertex1].z);
+    glVertex3f(vertexData[facesData[i].indexVertex2].x,
+               vertexData[facesData[i].indexVertex2].y,
+               vertexData[facesData[i].indexVertex2].z);
+    glVertex3f(vertexData[facesData[i].indexVertex3].x,
+               vertexData[facesData[i].indexVertex3].y,
+               vertexData[facesData[i].indexVertex3].z);
+    glVertex3f(vertexData[facesData[i].indexVertex4].x,
+               vertexData[facesData[i].indexVertex4].y,
+               vertexData[facesData[i].indexVertex4].z);
+  }
+  glEnd();  // END TRIANGLES DRAWING
 }
 
 // void OpenGlViewer::timerEvent(QTimerEvent *e) {}
