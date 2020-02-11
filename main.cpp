b341845f2d8d4a17c003744ea5b8f513bf1e87b3
Tony Eastwood <erlinmaquer@gmail.com>
2020-02-11 16:37:31 +0200

    [Fix] Fix creating empty object in storage.

--- main.cpp
+++ main.cpp
@@ -4,27 +4,28 @@
 #include "headers/mainwindow.h"
 #include "headers/objectManagement/objectparsemanager.h"
 #include "headers/objectManagement/objectstoragemanager.h"
-int main(int argc, char *argv[]) {
-  QApplication a(argc, argv);
-  qDebug() << "Start";
-  ObjectParseManager parser;
-  // Object3d testObject = parser.fromStlToObject("test.stl");
-  // testObject.setObjectName("myObj");
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    qDebug() << "Start";
+    ObjectParseManager parser;
+    // Object3d testObject = parser.fromStlToObject("test.stl");
+    // testObject.setObjectName("myObj");
 
-  Object3d testObject2 = parser.fromObjToObject("test.obj");
+    Object3d testObject = parser.fromObjToObject("test.obj");
 
-  ObjectStorageManager blabla("someDir");
-  blabla.addNewObject(testObject2);
-  qDebug() << "Vertex= " << testObject2.getQuantityVertex3d()
-           << "Line= " << testObject2.getQuantityLines3d()
-           << "Triangle= " << testObject2.getQuantityTriangles3d()
-           << "Faces= " << testObject2.getQuantityFaces3d();
-  Object3d qw;
-  MainWindow w;
-  MainAppWindow ew(testObject2);
-  ew.show();
-  w.show();
-  qDebug() << "End";
-  return a.exec();
-  // Vertex=  519577 Line=  1872738 Triangle=  624102 Faces=  108
+    ObjectStorageManager blabla("someDir");
+    blabla.addNewObject(testObject);
+
+    qDebug() << "Vertex= " << testObject.getQuantityVertex3d() << "Line= " << testObject.getQuantityLines3d()
+             << "Triangle= " << testObject.getQuantityTriangles3d()
+             << "Faces= " << testObject.getQuantityFaces3d();
+    Object3d qw;
+    MainWindow w;
+    MainAppWindow ew(testObject);
+    ew.show();
+    w.show();
+    qDebug() << "End";
+    return a.exec();
+    // Vertex=  519577 Line=  1872738 Triangle=  624102 Faces=  108
 }
